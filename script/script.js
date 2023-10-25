721e8db3e81345e6dee4809eb4590ae1c8b616ae
alejoalfonsoR <aa_lejo@hotmail.com>
2023-10-25 14:47:19 -0500

    Se sube el codigo completo

---
+++ script/script.js
@@ -0,0 +1,40 @@
+// Función principal para manejar el input y mostrar la sucesión
+function fibonacci() {
+    // Obtener elementos del DOM
+    const numeroInput = document.getElementById("numeroCalcular");
+    const mostrarResultado = document.getElementById("resultado");
+
+    // Obtener el valor del input y convertirlo a un número
+    const numero = parseInt(numeroInput.value);
+
+    // Validar si es un número
+    if (!isNaN(numero)) {
+        // Crear un array para almacenar la sucesión
+        const sucesion = [];
+
+        // Calcular y almacenar la sucesión
+        for (let i = 0; i < numero; i++) {
+            sucesion.push(calcularFibonacci(i));
+        }
+
+        // Mostrar la sucesión en el DOM
+        mostrarResultado.innerText = sucesion.join(", ");
+        numeroInput.value = "";
+    } else {
+        numeroInput.value = "";
+        mostrarResultado.innerText = "";
+        // Manejar el caso en que no se ingrese un número
+        alert(
+            "El valor ingresado no es un número. Por favor, ingrese un dato válido y vuelva a intentarlo."
+        );
+    }
+}
+
+// Función para calcular el número de Fibonacci de manera recursiva
+function calcularFibonacci(numero) {
+    if (numero <= 1) {
+        return numero;
+    } else {
+        return calcularFibonacci(numero - 1) + calcularFibonacci(numero - 2);
+    }
+}
